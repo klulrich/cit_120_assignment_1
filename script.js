b93ce5e06458436293a4be9f2d282275d8730dd5
klulrich <69543612+klulrich@users.noreply.github.com>
2020-08-31 11:30:28 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.querySelector("#cool").innerHTML="What's up bro?"
+
+var prompter = prompt("What is your name?");
+
+document.body.querySelector("#lessCool").innerHTML=prompter;
+
+var prompter2 = prompt("What is your favorite color?");
+
+document.body.querySelector("#ColorFool").innerHTML=prompter2;
+
+var prompter3 = prompt("What was your first video game?");
+
+document.body.querySelector("#firstGame").innerHTML=prompter3;
